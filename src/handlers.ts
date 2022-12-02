33379e279a1a9e58361c4dc902f02f76c8817403
Jesse Chounard <jessechounard@gmail.com>
2022-12-02 14:03:22 -0600

    Hardcoded webfinger and actor

---
+++ src/handlers.ts
@@ -0,0 +1,56 @@
+import { pogo } from "./deps.ts";
+
+export type Link = {
+  type: string;
+  rel?: string;
+  href?: string;
+};
+
+export type Webfinger = {
+  subject: string;
+  links: Link[];
+};
+
+export type Actor = {
+  n: number;
+};
+
+export async function webfingerHandler(
+  request: pogo.Request,
+  h: pogo.Toolkit
+): Promise<Webfinger> {
+  // todo: look at the query params and make sure they're even looking for this
+  return {
+    subject: "acct:jessechounard@tobefair.click",
+    links: [
+      {
+        rel: "self",
+        type: "application/activity+json",
+        href: "https://tobefair.click/actors/jessechounard",
+      },
+    ],
+  };
+}
+
+export async function actorRequest(
+  request: pogo.Request,
+  h: pogo.Toolkit
+): Promise<object> {
+  return {
+    "@context": [
+      "https://www.w3.org/ns/activitystreams",
+      "https://w3id.org/security/v1",
+    ],
+
+    id: "https://tobefair.click/actors/jessechounard",
+    type: "Person",
+    preferredUsername: "jessechounard",
+    inbox: "https://tobefair.click/inbox",
+
+    publicKey: {
+      id: "https://tobefair.click/actors/jessechounard#main-key",
+      owner: "https://tobefair.click/actors/jessechounard",
+      publicKeyPem: "-----BEGIN PUBLIC KEY-----...-----END PUBLIC KEY-----",
+    },
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { getConfig } from "./config.ts";
 import { pogo } from "./deps.ts";
+import { actorRequest, webfingerHandler } from "./handlers.ts";
+
+type webfinger = {
+  n: number;
+};
 
 const main = async () => {
   const config = await getConfig();
@@ -8,6 +13,14 @@ const main = async () => {
 
   server.router.get("/", () => {
     return "Hello, world!";
+  });
+
+  server.router.get("/.well-known/webfinger", (request, h) => {
+    return webfingerHandler(request, h);
+  });
+
+  server.router.get("/actors/{actor}", (request, h) => {
+    return actorRequest(request, h);
   });
 
   server.start();
